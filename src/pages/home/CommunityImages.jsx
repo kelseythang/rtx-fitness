4a568a790e9bfafced34ee9a376e25ae25f08b83
Kelsey Thang <thang.kelsey@gmail.com>
2023-09-09 21:13:57 -0400

    update img alts

--- src/pages/home/CommunityImages.jsx
+++ src/pages/home/CommunityImages.jsx
@@ -14,25 +14,25 @@ const CommunityImages = () => {
     <>
       { isMobile ? (
         <Carousel>
-          <img src={img1} alt='Fitness Image' />
-          <img src={img2} alt='Fitness Image' />
-          <img src={img3} alt='Fitness Image' />
-          <img src={img4} alt='Fitness Image' />
+          <img src={img1} alt='Gym Class' />
+          <img src={img2} alt='Gym Class' />
+          <img src={img3} alt='Gym Class' />
+          <img src={img4} alt='Gym Class' />
         </Carousel>
       ) : (
         <Container sx={{ display: 'flex', justifyContent: 'center' }}>
           <Grid container spacing={2}>
             <Grid>
-              <img className='img-slider' src={img1} alt='Fitness Image' />
+              <img className='img-slider' src={img1} alt='Gym Class' />
             </Grid>
             <Grid>
-              <img className='img-slider' src={img2} alt='Fitness Image' />
+              <img className='img-slider' src={img2} alt='Gym Class' />
             </Grid>
             <Grid>
-              <img className='img-slider' src={img3} alt='Fitness Image' />
+              <img className='img-slider' src={img3} alt='Gym Class' />
             </Grid>
             <Grid>
-              <img className='img-slider' src={img4} alt='Fitness Image' />
+              <img className='img-slider' src={img4} alt='Gym Class' />
             </Grid>
           </Grid>
         </Container>
